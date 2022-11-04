9b23d92185065c678c3ecb3a9bb04f82fd71f524
kylemondina <kylemondinaportfolio@gmail.com>
2022-11-04 01:14:29 -0700

    added README.md and cleaned up progress indication

--- parser.py
+++ parser.py
@@ -4,6 +4,8 @@ from instruction import Instruction
 
 if (len(sys.argv)<2):
 	print("incorrect use of program, please supply a file to be parsed")
+	print("example: ")
+	print("py parser.py <branch_trace.txt>")
 	exit()
 
 
@@ -11,29 +13,34 @@ instructions = []
 br_addrs = []
 btaken = 0
 bntaken = 0
-counter = 0
+progress = 0
 
 with open(sys.argv[1]) as f:
 	lines = f.readlines()
 	for line in lines:
-		counter = counter + 1	
-		print("parsing progress: " + str(counter))
+		progress = progress + 1
+		print("parsing in progress: " + str(round(progress/len(lines),4)) + " %")
+
 		attributes = line.split(" ")
+		
 		bi_addr = attributes[0]
+		br_addrs.append(attributes[0])
+
 		taken = attributes[1]
 		if (taken == "T"):
 			btaken = btaken + 1
 		elif(taken=="N"):
 			bntaken = bntaken + 1
+
 		tbi_addr = attributes[2]
 		instr_name = attributes[3]
+
 		instructions.append(Instruction(attributes[0],attributes[1],attributes[2],attributes[3]))
-		br_addrs.append(attributes[0])
-	
 
 
 distinct_br_addrs = Counter(br_addrs).keys()
-print("Distinct Branch Addresses " + str(len(distinct_br_addrs)))
+print("-------Parsing Complete-------")
+print("Distinct Branch Addresses: " + str(len(distinct_br_addrs)))
 print("Taken: " + str(btaken))
 print("Not Taken: " + str(bntaken))
 		
