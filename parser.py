e3f13382013e1fb8e2f0c0556c8d1cce70f4af19
kylemondina <kylemondinaportfolio@gmail.com>
2022-11-03 22:48:21 -0700

    completed parser, can take a test file and count how many of the instructions are taken

--- parser.py
+++ parser.py
@@ -1,11 +1,30 @@
 import sys
-
-
+from instruction import Instruction
 
 if (len(sys.argv)<2):
 	print("incorrect use of program, please supply a file to be parsed")
 	exit()
-	
-print("hi daddy")
 
+
+instructions = []
+btaken = 0
+
+with open(sys.argv[1]) as f:
+	lines = f.readlines()
+	for line in lines:
+		attributes = line.split(" ")
+		bi_addr = attributes[0]
+		taken = attributes[1]
+		tbi_addr = attributes[2]
+		instr_name = attributes[3]
+		instructions.append(Instruction(attributes[0],attributes[1],attributes[2],attributes[3]))
+
+for instruction in instructions:
+	print(instruction.taken)
+	if (instruction.taken == "T"):
+		btaken = btaken + 1
+
+print(btaken)
+
+		
 
