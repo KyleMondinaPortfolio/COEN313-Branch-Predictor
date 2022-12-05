2796f17576760e363fd5a9e605cec57c6d99ceed
unknown <justinwli.jj6@gmail.com>
2022-12-04 21:34:58 -0800

    added 10-bit address to tourney predictor

--- phase4/LocalPredictor.py
+++ phase4/LocalPredictor.py
@@ -1,5 +1,5 @@
 import sys
-from hasher import hasher5
+from hasher import hasher5, hasher10
 from TournamentPredictor import TournamentPredictor
 
 if (len(sys.argv)<2):
@@ -10,13 +10,15 @@ if (len(sys.argv)<2):
 
 progress = 0
 
-#dictionaries for full and five-bit branch addresses
+#dictionaries for full, 5-bit, and 10-bit branch addresses
 full = {}
 five = {}
+ten = {}
 
 #initializing misprediction counters
 mp = 0
 mp5 = 0
+mp10 = 0
 
 
 with open(sys.argv[1]) as f:
@@ -32,9 +34,13 @@ with open(sys.argv[1]) as f:
         if bi_addr not in full:
             full[bi_addr]=TournamentPredictor()
             
-            hashed_bi_addr = hasher5(bi_addr)	
-            if hashed_bi_addr not in five:
-                five[hashed_bi_addr]=TournamentPredictor()
+            hashed_bi_addr_5 = hasher5(bi_addr)	
+            if hashed_bi_addr_5 not in five:
+                five[hashed_bi_addr_5]=TournamentPredictor()
+
+            hashed_bi_addr_10 = hasher10(bi_addr)	
+            if hashed_bi_addr_10 not in ten:
+                ten[hashed_bi_addr_10]=TournamentPredictor()
         
                 
         #Check if the Branch is Taken or Not
@@ -50,16 +56,22 @@ with open(sys.argv[1]) as f:
         if (full[bi_addr].predict() != is_taken):
             mp = mp + 1
 
-        if (five[hashed_bi_addr].predict() != is_taken):
+        if (five[hashed_bi_addr_5].predict() != is_taken):
             mp5 = mp5 + 1
+
+        if (ten[hashed_bi_addr_10].predict() != is_taken):
+            mp10 = mp10 + 1
 
         #Update The Branch Register
         full[bi_addr].update(is_taken)
-        five[hashed_bi_addr].update(is_taken)
+        five[hashed_bi_addr_5].update(is_taken)
+        ten[hashed_bi_addr_10].update(is_taken)
 
 
 print("-------Parsing Complete-------")
-print ("(5,1) Full Addr Local Predictor Miss Rate: " + str(round(mp/progress * 100.0,2)) + "%")
-print ("(5,1) 5-bit Addr Local Predictor Miss Rate: " + str(round(mp5/progress * 100.0,2)) + "%")
+print ("5-bit Addr Tourney Predictor Miss Rate: " + str(round(mp5/progress * 100.0,2)) + "%")
+print ("10-bit Addr Tourney Predictor Miss Rate: " + str(round(mp10/progress * 100.0,2)) + "%")
+print ("Full Addr Tourney Predictor Miss Rate: " + str(round(mp/progress * 100.0,2)) + "%")
+
         
 
